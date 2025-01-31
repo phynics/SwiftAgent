834e07e0663a2e37f4825901881ebadedd184a27
Atakan DULKER <contact@atkn.me>
2025-01-31 20:27:14 +0100

    feat: removed anthropic and distributed agents

--- Package.swift
+++ Package.swift
@@ -22,8 +22,6 @@ let package = Package(
         .package(url: "https://github.com/kevinhermawan/swift-json-schema.git", branch: "main"),
         .package(url: "https://github.com/kevinhermawan/swift-llm-chat-openai.git", branch: "main"),
         .package(url: "https://github.com/1amageek/OllamaKit.git", branch: "main"),
-        .package(url: "https://github.com/apple/swift-distributed-actors.git", branch: "main"),
-        .package(url: "https://github.com/jamesrochabrun/SwiftAnthropic.git", branch: "main")
     ],
     targets: [
         .target(
@@ -37,7 +35,6 @@ let package = Package(
             dependencies: [
                 "SwiftAgent",
                 "AgentTools",
-                "SwiftAnthropic",
                 "OllamaKit",
                 .product(name: "LLMChatOpenAI", package: "swift-llm-chat-openai"),
             ]
@@ -46,20 +43,12 @@ let package = Package(
             name: "AgentTools",
             dependencies: ["SwiftAgent"]
         ),
-        .target(
-            name: "AgentActor",
-            dependencies: [
-                "SwiftAgent",
-                .product(name: "DistributedCluster", package: "swift-distributed-actors")
-            ]
-        ),
         .executableTarget(
             name: "AgentCLI",
             dependencies: [
                 "SwiftAgent",
                 "AgentTools",
                 "Agents",
-                "SwiftAnthropic",
                 "OllamaKit",
                 .product(name: "LLMChatOpenAI", package: "swift-llm-chat-openai"),
                 .product(name: "ArgumentParser", package: "swift-argument-parser")
--- Sources/AgentCLI/MainAgent.swift
+++ Sources/AgentCLI/MainAgent.swift
@@ -15,7 +15,7 @@ public struct MainAgent: Agent {
     public var body: some Step<String, String> {
         Loop { _ in
             WaitForInput(prompt: "You: ")
-            AnthropicAgent()
+            OpenAIAgent()
                 .onInput { message in
                     print(message)
                 }
